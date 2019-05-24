e7a3a3339623fd67d9cccbb425c3f4b2c9a68058
Bishal Bhattacharjee <gullu953@gmail.com>
2019-05-24 08:56:43 +0530

    hfh

--- src/components/adminpanel/CreatePost.js
+++ src/components/adminpanel/CreatePost.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import "./Adminpanel.css";
+import "./AdminPanel.css";
 import AdminDashSidebar from "./AdminDashSidebar";
 
 export class CreatePost extends Component {
